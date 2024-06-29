839bd51add434cbdd7789aa38a8436551cc24844
Bridget <bridget.njoki@student.moringaschool.com>
2024-06-30 01:28:22 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2);
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = functions(drivers, whichDrivers){
+    return whichDrivers(drivers);
+};
